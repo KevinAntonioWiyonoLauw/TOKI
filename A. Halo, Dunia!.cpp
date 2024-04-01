3b5a2d167eadaa5000f76933d88d395405d408f7
Kevin <keviniogt02@gmail.com>
2024-04-01 09:10:39 +0800

    first commit

---
+++ A. Halo, Dunia!.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define ll long long 
+#define endl '\n'
+#define pb push_back
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define eb emplace_back
+#define test int t; cin>>t; while(t--)
+#define tr(k,st,en) for(int k = st; k <= en ; k++)
+#define trb(k,en,st) for(int k = en; k >= st ; k--)
+using namespace std;
+ 
+typedef pair<int, int> pr;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pr> vpr;
+
+//const int mod = 1e9+7;
+//const int inf = 9e18;
+ 
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+ 
+    cout << "Halo, dunia!";
+ 
+  return 0;
+}
